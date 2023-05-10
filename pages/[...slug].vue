357a6b39c30f052c9ad031b66ce5e0b52521213f
Remi  PLANEL <rplanel@pasteur.fr>
2023-05-10 18:17:06 +0200

    remove v-container

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,12 +1,10 @@
 <template>
-  <v-container>
-    <v-row justify="center">
-      <v-col cols="auto">
-        <v-card flat color="transparent" max-width="1280">
-          <v-card-text>
-            <ContentDoc />
-          </v-card-text> </v-card
-      ></v-col>
-    </v-row>
-  </v-container>
+  <v-row justify="center">
+    <v-col cols="auto">
+      <v-card flat color="transparent" max-width="1280">
+        <v-card-text>
+          <ContentDoc />
+        </v-card-text> </v-card
+    ></v-col>
+  </v-row>
 </template>
