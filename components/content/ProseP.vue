4d4af82a39e84f2acb15a0cc4e693f3ed7cd5d35
Remi  PLANEL <rplanel@pasteur.fr>
2023-05-10 17:05:16 +0200

    Use p for proseP

--- components/content/ProseP.vue
+++ components/content/ProseP.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-card flat color="transparent">
+  <!-- <v-card flat color="transparent">
     <v-card-text class="px-0 py-2"><slot /></v-card-text>
-  </v-card>
+  </v-card> -->
+  <p><slot /></p>
 </template>
